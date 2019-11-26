31983a3cdab3c12b78f4a438434fe405a4d6c551
Stephanie <46434206+tsuyunoinochi@users.noreply.github.com>
2019-11-25 20:11:19 -0500

    Add files via upload

--- selection.cpp
+++ selection.cpp
@@ -9,9 +9,6 @@ using namespace std;
 
 void selection()
 {
-	mt19937 rng;
-	rng.seed(random_device()());
-	uniform_int_distribution<std::mt19937::result_type> distrForPrizes(0, 23);
 	//adding the files into the program
 	//"prizes1" contains the first 24 prizes
 	ifstream prizes("prizes1.txt", ios::in);
@@ -21,8 +18,8 @@ void selection()
 	vector<string> studentNameVector; 
 	vector<string> prizeWinnerVector; 
 	string line; 
-	string tempPrizeArray[4]; 
-	string tempStudentArray[4];
+	string tempPrizeArray[5]; 
+	string tempStudentArray[5];
 
 	//check if files are valid before proceeding; print some kind of message if there's an error
 	if (!prizes || !student_names || !winners)
@@ -37,7 +34,7 @@ void selection()
 			if (line.size() > 0)
 			{
 				prizeVector.push_back(line);
-				cout << line << endl; 
+				//cout << line << endl; 
 			}
 		}
 
@@ -46,37 +43,40 @@ void selection()
 			if (line.size() > 0)
 			{
 				studentNameVector.push_back(line);
-				cout << line << endl;
+				//cout << line << endl;
 			}
 		}
-
-		uniform_int_distribution<std::mt19937::result_type> distrForAttendees(0, studentNameVector.size());
 
 		while (getline(winners, line))
 		{
 			if (line.size() > 0)
 			{
 				prizeWinnerVector.push_back(line);
-				cout << line << endl;
+				//cout << line << endl;
 			}
 		}
 
 		//find 5 random winners
-		for (int i = 0; i < 4; i++)
+		for (int i = 0; i < 5; i++)
 		{
-			//tempStudentArray[i] = studentNameVector[distrForAttendees];
-			tempStudentArray[i] = studentNameVector
+			tempStudentArray[i] = studentNameVector[rand() % studentNameVector.size()];
 			cout << tempStudentArray[i] << endl;
 
-		}
+			for (int j = 0; j < tempStudentArray->length; j++)
+			{
+				if (tempStudentArray[j] == tempStudentArray[i])
+				{
 
+				}
+			}
+		}
 
 		//step 1: prizeVector[distrForPrizes]; 
 		//step 2: check if the called prize is in the temp array
 		//step 3: if not, add called prize to temp array
 		//step 4: call the next random number
 
-		//check winner name against the 'prizeWinners' file; if it's there, delete name from 'studentNames' folder & pick another name. Test again, until name is no longer a duplicate
+		//check winner name against the 'prizeWinners' file; if it's there, delete name from 'studentNames' file & pick another name. Test again, until name is no longer a duplicate
 		//write their names into the 'prizeWinners' file
 		//delete their names from the 'studentNames' file
 
